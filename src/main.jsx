75eca704bf3bceb4bfd58be98f6b9adc4cb74e1c
Ringich <dinsam888@gmail.com>
2025-12-25 22:24:20 +0500

    Created a link between Start->Lobby->Active pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StartPage from "./pages/Start_Page.jsx"
-import GameLobby from "./pages/Game_Lobby.jsx";
-import GameActive from "./pages/Game_Active.jsx";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 import "./main_page.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GameActive />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
